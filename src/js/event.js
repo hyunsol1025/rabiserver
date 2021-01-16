caea549906b0dd16d031811ac349748b080d783a
BuildTools <unconfigured@null.spigotmc.org>
2021-01-16 13:54:24 +0900

    웹 업데이트

---
+++ src/js/event.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    document.getElementById('head-line-content').style.opacity = 1;
+}
+
+window.addEventListener('resize',function() {
+   var ele = document.getElementById('head-line-content');
+    
+   if(document.body.offsetWidth <= 850){
+       ele.src = "../src/img/logo2.png";
+   } else {
+       ele.src = "../src/img/logo.png";
+   }
+});
---
+++ src/js/function.js
@@ -0,0 +1,3 @@
+function gotoId(id) {
+    document.getElementById(id).scrollIntoView();
+}
