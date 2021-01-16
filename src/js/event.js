044fe8546f12875f5a447ae0fe871e791ecefa1d
BuildTools <unconfigured@null.spigotmc.org>
2021-01-16 13:58:07 +0900

    Update event.js
    ddd

--- src/js/event.js
+++ src/js/event.js
@@ -6,8 +6,8 @@ window.addEventListener('resize',function() {
    var ele = document.getElementById('head-line-content');
     
    if(document.body.offsetWidth <= 850){
-       ele.src = "../src/img/logo2.png";
+       ele.src = "./src/img/logo2.png";
    } else {
-       ele.src = "../src/img/logo.png";
+       ele.src = "./src/img/logo.png";
    }
 });
