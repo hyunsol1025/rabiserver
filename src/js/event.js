c70abd62577a09e6c02c078b8bba04604c8e4757
BuildTools <unconfigured@null.spigotmc.org>
2021-01-18 22:48:23 +0900

    dd
    ddddd

--- src/js/event.js
+++ src/js/event.js
@@ -1,5 +1,20 @@
 window.onload = function () {
     document.getElementById('head-line-content').style.opacity = 1;
+    
+    
+    // 류아 -------
+    document.getElementById('HELLOWORLD').addEventListener("click",function() {
+        var ele = document.getElementById('HELLOWORLD');
+        
+        if(ele.src.includes("lyua2.png")){
+            ele.src = "./src/img/lyua.png";
+            document.getElementById('HELLOWORLD_TXT').style.display = "none";
+            return;
+        }
+        
+        ele.src = "./src/img/lyua2.png";
+        document.getElementById('HELLOWORLD_TXT').style.display = "block";
+    });
 }
 
 window.addEventListener('resize',function() {
@@ -10,4 +25,34 @@ window.addEventListener('resize',function() {
    } else {
        ele.src = "./src/img/logo.png";
    }
+});
+
+var nextKeyNum = 76;
+function Lyua() {
+    var ele = document.getElementById("HELLOWORLD");
+    ele.style.display = "block";
+    ele.style.opacity = 1;
+    ele.style.height = 200;
+}
+
+window.addEventListener("keydown", function(e) {
+    if(e.keyCode == nextKeyNum){
+        switch(nextKeyNum){
+            case 76:
+                nextKeyNum = 89;
+                break;
+            case 89:
+                nextKeyNum = 85;
+                break;
+            case 85:
+                nextKeyNum = 65;
+                break;
+            case 65:
+                Lyua();
+                break;
+        }
+    } else {
+        nextKeyNum = 76;
+    }
+    // 키 순서: 76 89 85 65 91
 });
